4f40a72dd1cb1c4377d9c66ceec305ba94495a06
Littlemeow0122 <nicchen0122@gmail.com>
2026-04-12 01:45:05 +0000

    Update 2026-04-12 01:45:05

--- publish.sh
+++ publish.sh
@@ -1,18 +1,11 @@
 #1
-
 if git diff --quiet && git diff --staged --quiet; then
     echo "沒有更改需要提交。"
     exit 0
 fi
-
 git add .
-
 commit_message="Update $(date +'%Y-%m-%d %H:%M:%S')"
 git commit -m "$commit_message"
 git pull origin main --rebase
-
-
-
 git push origin main
-
 echo "已成功發佈到 GitHub！"
